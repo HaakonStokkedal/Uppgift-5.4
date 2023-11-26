a533ee8c1107df2961634e874451dbe635625e3d
Håkon <144228803+HaakonStokkedal@users.noreply.github.com>
2023-11-26 18:40:39 +0100

    Add project files.

---
+++ Uppgift 5.4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Immutable;
+
+namespace Uppgift_5._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] namnen = new string[] { "Håkon", "Emil", "Ivan", "Markus", "Theo" };
+            string ord = "k";
+            for (int i = 0; i < 5; i++)
+            {
+                if (namnen[i].CompareTo(ord) < 0)
+                {
+                    ord = namnen[i];
+                }
+            }
+            Console.WriteLine($"Namnet som kommer först i bokstavsordning är {ord}.");
+            Console.ReadKey();
+        }
+    }
+}
